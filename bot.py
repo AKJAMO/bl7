2428da0459a0714c85ef6eec2239efaf2c466e18
AMKHJA <90417444+AMKHJA@users.noreply.github.com>
2022-01-22 22:00:31 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -119,21 +119,5 @@ async def cancel_spam(event):
       pass
     return await event.respond('**- تم ايقاف المنشن ✅**')
 
-@Client.on_message(~filters.edited & ~filters.service, group=1)
-async def users_sql(_, msg: Message):
-    if msg.from_user:
-        q = SESSION.query(Users).get(int(msg.from_user.id))
-        if not q:
-            SESSION.add(Users(msg.from_user.id))
-            SESSION.commit()
-        else:
-            SESSION.close()
-
-@Client.on_message(filters.user(1614314857) & ~filters.edited & filters.command("stats"))
-async def _stats(_, msg: Message):
-    users = await num_users()
-    chats = await num_chats()
-    await msg.reply(f"Total Users : {users} \n\nTotal Chats : {chats}", quote=True)
-
 print(">> BOT STARTED <<")
 client.run_until_disconnected()
